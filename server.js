438cc6c1f8240ed155c406eb5180deb449121118
Maxence Bouret <maxence@ironhack.com>
2018-06-10 21:48:57 +0200

    Add project page

--- server.js
+++ server.js
@@ -66,6 +66,13 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects Page',
+		welcomeMessage: 'This is my projects'
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage : 'Unable to handle request'
